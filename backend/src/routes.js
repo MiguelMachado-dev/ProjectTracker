5dd74112ac877305d2df8e144c588a4886328e6e
Miguel Machado <miiguellmp@gmail.com>
2019-06-14 23:57:55 -0300

    route to check api health and create user

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.send('Server on!')
-})
+const UserController = require('./app/controllers/UserController')
+
+routes.get('/api', (req, res) =>
+  res.status(200).send({
+    message: 'Server on!'
+  })
+)
+
+routes.post('/users', UserController.store)
 
 module.exports = routes
