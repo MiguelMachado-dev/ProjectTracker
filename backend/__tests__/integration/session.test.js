829402590280ffad0456053cccda72b52e4d3cbe
Miguel Machado <miiguellmp@gmail.com>
2019-06-16 14:07:41 -0300

    testing tokens and auth

--- backend/__tests__/integration/session.test.js
+++ backend/__tests__/integration/session.test.js
@@ -44,4 +44,51 @@ describe('Authentication', () => {
 
     expect(response.status).toBe(401)
   })
+
+  it('should return jwt token when authenticated', async () => {
+    const user = await User.create({
+      name: 'Miguel',
+      email: 'miguel2@gmail.com',
+      password: '123123',
+      superuser: false
+    })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123123'
+      })
+
+    expect(response.body).toHaveProperty('token')
+  })
+
+  it('should be able to access private routes when authenticated', async () => {
+    const user = await User.create({
+      name: 'Miguel',
+      email: 'miguel2@gmail.com',
+      password: '123123',
+      superuser: false
+    })
+
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer ${user.generateToken()}`)
+
+    expect(response.status).toBe(200)
+  })
+
+  it('should not be able to access private routes when not authenticated', async () => {
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', 'Bearer 2133123')
+
+    expect(response.status).toBe(401)
+  })
+
+  it('should not be able to access private routes when not authenticated', async () => {
+    const response = await request(app).get('/dashboard')
+
+    expect(response.status).toBe(401)
+  })
 })
