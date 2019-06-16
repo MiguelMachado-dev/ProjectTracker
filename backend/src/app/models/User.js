07d28c0c6b0c92212ed6df13d90bcdd9b0db12f3
Miguel Machado <miiguellmp@gmail.com>
2019-06-16 16:26:47 -0300

    user jwt with appsecret

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
   }
 
   User.prototype.generateToken = function () {
-    return jwt.sign({ id: this.id }, 'shhhhhhh')
+    return jwt.sign({ id: this.id }, process.env.APP_SECRET)
   }
 
   return User
