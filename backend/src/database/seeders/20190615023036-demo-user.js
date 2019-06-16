72c01402f33783d2305f8b8fa45c3c38c024ff21
Miguel Machado <miiguellmp@gmail.com>
2019-06-15 22:28:07 -0300

    seed to populate users table

--- backend/src/database/seeders/20190615023036-demo-user.js
+++ backend/src/database/seeders/20190615023036-demo-user.js
@@ -6,10 +6,26 @@ module.exports = {
       'users',
       [
         {
-          name: 'Teste',
-          email: 'admin2@gmail.com',
+          name: 'Administrador',
+          email: 'admin@gmail.com',
           password_hash: Math.random().toString(),
           superuser: true,
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          name: 'Programador 1',
+          email: 'coder1@gmail.com',
+          password_hash: Math.random().toString(),
+          superuser: false,
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          name: 'Programador 2',
+          email: 'coder2@gmail.com',
+          password_hash: Math.random().toString(),
+          superuser: false,
           created_at: new Date(),
           updated_at: new Date()
         }
