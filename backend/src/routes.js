dff52aaac4c210cf6dbd956d9a6ac274583fedbb
Miguel Machado <miiguellmp@gmail.com>
2019-06-16 14:07:15 -0300

    using middlewares to auth

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
+const authMiddleware = require('./app/middlewares/auth')
+
 routes.get('/api', (req, res) =>
   res.status(200).send({
     message: 'Server on!'
@@ -14,4 +16,9 @@ routes.get('/api', (req, res) =>
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 
+routes.use(authMiddleware) // every routes below this line will use auth
+
+routes.get('/dashboard', (req, res) => {
+  return res.status(200).send()
+})
 module.exports = routes
