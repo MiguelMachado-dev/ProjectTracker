3db4d7dc4593e9f50db4ff77911315414eb34b58
Miguel Machado <miiguellmp@gmail.com>
2019-06-16 16:27:29 -0300

    removing console.log

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -3,7 +3,6 @@ const { User } = require('../models')
 class UserController {
   async store (req, res) {
     const user = await User.create(req.body)
-    console.log(user)
     return res.json(user)
   }
 }
